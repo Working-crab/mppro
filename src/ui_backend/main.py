2ec4d83eb5cbcdf73dc50a6c7e2bcc4ed847bcc6
root <root@aneymik0.fvds.ru>
2023-01-22 20:26:07 +0300

    add decorator

--- src/ui_backend/main.py
+++ src/ui_backend/main.py
@@ -1,16 +1,27 @@
 
+import json
 import telebot
 import requests
 import re
 
-from src.ui_backend.db.queries import db_queries
-from src.wb_common.wb_queries import wb_queries
+from ui_backend.db.queries import db_queries
+from wb_common.wb_queries import wb_queries
 
 BOT_NAME = 'mp_pro_bot'
 TOKEN = '5972133433:AAERP_hpf9p-zYjTigzEd-MCpQWGQNCvgWs'
 
 #Создание бота
 bot = telebot.TeleBot(TOKEN)
+
+def try_except_decorator(fn):
+    
+    def the_wrapper(message):
+        try:
+            bot.send_message(message.chat.id, fn(message))
+        except Exception as e:
+            bot.send_message(message.chat.id, f'Произошла ошибка: {type(e).__name__}: {e}')
+
+    return the_wrapper
 
 #Команды бота
 @bot.message_handler(commands=['start'])
@@ -41,18 +52,17 @@ def getToken(message):
 @bot.message_handler(commands=['search'])
 def search(message):
     try:
-        query = re.sub('/search ', '', message.text)
-        r2 = requests.get(f'https://catalog-ads.wildberries.ru/api/v5/search?keyword={query}')
-        item_dicts = r2.json()['adverts']
-        item_dicts = sorted(item_dicts, key=lambda dict_item: dict_item.get('cpm'), reverse=True)[0:10]
+        keyword = re.sub('/search ', '', message.text)
+        item_dicts = wb_queries.search_adverts_by_keyword(keyword)
         result_message = ''
 
         if len(item_dicts) == 0:
             return bot.send_message(message.chat.id, 'ставки неизвестны')
         else:
             for item_idex in range(len(item_dicts)):
-                price = item_dicts[item_idex]['cpm']
-                p_id = item_dicts[item_idex]['id']
+                price = item_dicts[item_idex]['price']
+                p_id = item_dicts[item_idex]['p_id']
+                # TODO Сделать вывод по-красивше
                 result_message += f'{item_idex + 1})  {price}р,  https://www.wildberries.ru/catalog/{p_id}/detail.aspx \n'
             bot.send_message(message.chat.id, result_message)
 
@@ -63,40 +73,33 @@ def search(message):
 @bot.message_handler(commands=['list_atrevds'])
 def list_atrevds(message):
   try:
-    user_cmp_token = db_queries.get_user_wb_cmp_token(telegram_user_id=message.from_user.id)
+    user = db_queries.get_user_by_telegram_user_id(message.from_user.id)
+    user_wb_tokens = wb_queries.get_base_tokens(user)
+    req_params = wb_queries.get_base_request_params(user_wb_tokens)
 
-    cookies = {
-      'WBToken': user_cmp_token,
-      'x-supplier-id-external': 'f05df88e-0b40-462e-9d55-753712a8a59b'
-    }
-
-    headers = {
-      'X-User-Id': '61712490',
-      'Referer': 'https://cmp.wildberries.ru/campaigns/list/all',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36',
-    }
-
-    status_dict = {
+    status_dict = { # TODO УБРАТЬ ЭТОТ СРАМ!
       '4': 'Готова к запуску',
-      '11': 'Приостановлено',
       '9': 'Активна',
+      '8': 'Отказана',
+      '11': 'Приостановлено',
     }
 
-    user_atrevds = requests.get('https://cmp.wildberries.ru/backend/api/v3/atrevds?order=createDate', cookies=cookies, headers=headers)
+    # TODO Вынести запрос в wb_queries
+    user_atrevds = requests.get('https://cmp.wildberries.ru/backend/api/v3/atrevds?order=createDate', cookies=req_params['cookies'], headers=req_params['headers'])
     view = user_atrevds.json()['content']
     result_msg = ''
 
     for product in view:
         date_str = product['startDate']
         
-        stat = status_dict[str(product['statusId'])]
+        stat = status_dict.get(product['statusId'], 'Статус не известен')
         if date_str != None:
             date_str = date_str[:10]
         
-        result_msg += f"{product['categoryName']}  {product['campaignName']}  {product['campaignId']}  {date_str} {stat}\n"
+        # TODO Сделать вывод по-красивше
+        result_msg += f"{product['id']} \t {product['categoryName']} \t {product['campaignName']} \t {product['campaignId']} \t {date_str} {stat}\n"
     
 
-    # user_atrevds: parse me
     bot.send_message(message.chat.id, result_msg)
 
   except Exception as e:
@@ -115,12 +118,19 @@ def add_advert(message):
         user = db_queries.get_user_by_telegram_user_id(message.from_user.id)
 
         #(индентификатор, бюджет, место которое хочет занять)args*
-        compagin_id = re.sub('/add_advert ', 'ЖОПА', message.text)
-        max_budget = re.sub('/add_advert ', '', message.text)
-        place = re.sub('/add_advert ', '', message.text)
+        message_args = re.sub('/add_advert ', '', message.text).split(sep=' ', maxsplit=4)
+        if len(message_args) != 4:
+          bot.send_message(message.chat.id, f'Для использования команды используйте формат: /add_advert <campaign_id> <max_budget> <place> <status>')
+          return
 
-        advert = db_queries.add_user_advert(user, compagin_id, max_budget, place)
-        bot.send_message(message.chat.id, advert)
+        compagin_id = re.sub('/add_advert ', '', message_args[0])
+        max_budget = re.sub('/add_advert ', '', message_args[1])
+        place = re.sub('/add_advert ', '', message_args[2])
+        status = re.sub('/add_advert ', '', message_args[3])
+
+        db_queries.add_user_advert(user, status, compagin_id, max_budget, place)
+        bot.send_message(message.chat.id, 'Ваша рекламная компания успешно добавлена!')
+        
     except Exception as e:
         bot.send_message(message.chat.id, f'Произошла ошибка: {e}')
 
@@ -130,8 +140,13 @@ def get_adverts(message):
     try:
         user = db_queries.get_user_by_telegram_user_id(message.from_user.id)
         adverts = db_queries.get_user_adverts(user.id)
+
+        result = json.dumps(adverts)
+
+        if not adverts:
+          result = 'Вы ещё не добавили нам команий! Используйте add_advert'
         
-        bot.send_message(message.chat.id, adverts)
+        bot.send_message(message.chat.id, result)
     except Exception as e:
         bot.send_message(message.chat.id, f'Произошла ошибка: {e}')
 
@@ -147,6 +162,17 @@ def reset_base_tokens(message):
     except Exception as e:
         bot.send_message(message.chat.id, f'Произошла ошибка: {e}')
 
+@bot.message_handler(commands=['test'])
+@try_except_decorator
+def print_test(message):
+    if len(message.text.split()) > 1:
+        return message
+    else:
+        raise Exception('Ахуеть какая ебейшая ошибка и сообщение пиздатое и правильное')
+
+# TODO Сделать хелп команду
+
+# TODO Сделать общий абстрактный декоратор
 
 #Начало пулинга
 bot.polling(interval=3, none_stop=True)
