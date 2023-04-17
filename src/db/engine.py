80b0689b3c262748afe8573fdd9c376405a3dcdb
Suslicke <suslicketeam@gmail.com>
2023-04-17 19:54:56 +0400

    integrated chat_gpt in tg bot

--- src/db/engine.py
+++ src/db/engine.py
@@ -6,5 +6,5 @@ logging.basicConfig()
 logging.getLogger('sqlalchemy').setLevel(logging.ERROR)
 logging.disable(logging.INFO)                   
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres", future=True, echo=True) # , echo=True
+engine = create_engine("postgresql://postgres:postgres@localhost:5434/postgres", future=True, echo=True) # , echo=True
 Base.metadata.create_all(engine)
