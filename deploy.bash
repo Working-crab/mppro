f7b75e87a97366c23e8efb5d4a9efa056fddf51e
Andrei Suslicke <51888863+Suslicke@users.noreply.github.com>
2023-05-04 15:36:55 +0300

    Update and rename deploy.sh to deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -6,4 +6,5 @@ docker compose -f /data/docker-compose.yml up -d && \
 cd $source_dir && \
 git fetch && git pull && \
 pip install -r python_requirements.txt && \
+pm2 kill \
 pm2 start pm2.config.js
