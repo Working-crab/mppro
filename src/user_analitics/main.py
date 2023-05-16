9d6fa4e4b9c66d02e52b93e6ea0e938a8a7c5a18
Lev Sergeev <levushka46@mail.ru>
2023-05-16 23:05:30 +0300

    Add user_id and campaign_id into user_analitics

--- src/user_analitics/main.py
+++ src/user_analitics/main.py
@@ -3,4 +3,4 @@ from datetime import time
 from user_analitics.analitics_automation import analitics_automation
 
 def start_logs_analitcs(user_id, campaign_id):
-    analitics_automation.start(user_id,campaign_id)
+    analitics_automation.start(user_id, campaign_id)
