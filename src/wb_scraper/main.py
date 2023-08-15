4cc5dc996ea0973fa1157ff71ba55447881c67f6
NANDIorg <super.egor.mamonov@yandex.ru>
2023-08-15 19:14:54 +0700

    fix refactoring

--- src/wb_scraper/main.py
+++ src/wb_scraper/main.py
@@ -8,7 +8,7 @@ script_name = argv[1]
 
 if __name__ == '__main__':
     if script_name == "search_word":
-        scrapping_search_word(argv[2],argv[3])
+        scrapping_search_word(int(argv[2]),int(argv[3]))
     pass
 
 print(time.time()-start)
