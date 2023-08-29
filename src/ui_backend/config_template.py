13ae070b6008dd35275ef445289c62e0780967f7
Suslicke <suslicketeam@gmail.com>
2023-08-29 11:54:41 +0300

    .

--- src/ui_backend/config_template.py
+++ src/ui_backend/config_template.py
@@ -1,11 +1,12 @@
 BOT_NAME = 'admp_pro_test_bot'
-TOKEN = '000'
-WEBHOOK_URL = '000'
+TOKEN = '6014923966:AAGnsVeVehXuLG1fKoTbOrbRwZWUrwJ_jto'
+WEBHOOK_URL = 'https://stage.admp.pro/'
 GPT_TOKEN = '000'
 GPT_MODEL_NAME = 'gpt-3.5-turbo'
-JWT_TOKEN = "000"
-ALGORITHM = "000"
 
-yookassa_account_id = 000
-yookassa_secret_key = '000'
-PAYMENT_TOKEN = '000'
+JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+ALGORITHM = "HS256"
+
+yookassa_account_id = 980339
+yookassa_secret_key = 'test_Ue2xPoNd6OaYiH2jogyGl1H0PenP_xB993f3nqTyxj4'
+PAYMENT_TOKEN = '381764678:TEST:49601'
