e4e46a15dc90ade8fc25776b2b399a2acba81f65
Suslicke <suslicketeam@gmail.com>
2023-08-29 11:36:13 +0300

    update config_template

--- src/ui_backend/config_template.py
+++ src/ui_backend/config_template.py
@@ -1,14 +1,11 @@
-BOT_NAME = 'mp_pro_bot'
-TOKEN = '6014923966:AAGnsVeVehXuLG1fKoTbOrbRwZWUrwJ_jto'
-WEBHOOK_URL = 'https://stage.admp.pro/'# урл домена
-GPT_TOKEN = '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' #
+BOT_NAME = 'admp_pro_test_bot'
+TOKEN = '000'
+WEBHOOK_URL = '000'
+GPT_TOKEN = '000'
 GPT_MODEL_NAME = 'gpt-3.5-turbo'
-yookassa_account_id = 980339 #Тест версия
-yookassa_secret_key = 'test_Ue2xPoNd6OaYiH2jogyGl1H0PenP_xB993f3nqTyxj4' #Тест версия
-PAYMENT_TOKEN = '381764678:TEST:49601' #Тест
-JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-ALGORITHM = "HS256"
+JWT_TOKEN = "000"
+ALGORITHM = "000"
 
-# Configuration.account_id = 978902 #Боевой
-# Configuration.secret_key = 'live_l0GbasWj_Beak-mP0iScmjYPCEUuUtua6FpyyGlM6zQ' #Боевой
-# PAYMENT_TOKEN = '390540012:LIVE:30668' #Боевой
+yookassa_account_id = 000
+yookassa_secret_key = '000'
+PAYMENT_TOKEN = '000'
